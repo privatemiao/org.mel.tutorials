d0d022303bb16eee2f9a4e04cb84c503c07e875f
Mel Liu <privatemiao@gmail.com>
2016-05-14 02:43:32 +0800

    a

---
+++ tutorial-shiro/src/test/java/org/mel/tutorial/shiro/ShiroSimpleTest.java
@@ -0,0 +1,20 @@
+package org.mel.tutorial.shiro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShiroSimpleTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
