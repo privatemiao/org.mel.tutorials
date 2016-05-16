a3d294ac5f9c83a3ba05ed392db121bf80af74d6
Mel Liu <privatemiao@gmail.com>
2016-05-16 15:19:56 +0800

    a

--- tutorial-shiro/src/test/java/org/mel/tutorial/shiro/ShiroSimpleTest.java
+++ tutorial-shiro/src/test/java/org/mel/tutorial/shiro/ShiroSimpleTest.java
@@ -1,7 +1,11 @@
 package org.mel.tutorial.shiro;
 
-import static org.junit.Assert.*;
-
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,12 +13,16 @@ public class ShiroSimpleTest {
 
 	@Before
 	public void setUp() throws Exception {
-		
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-simple.ini");
+		SecurityUtils.setSecurityManager(factory.getInstance());
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Subject currentUser = SecurityUtils.getSubject();
+		currentUser.login(new UsernamePasswordToken("privatemiao", "P@ssw0rd"));
+		System.out.println(currentUser.isPermitted("system:100:role:create"));
+		currentUser.logout();
 	}
 
 }
