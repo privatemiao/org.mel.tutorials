fe9f9a464dd57533228295bbc550c899f4cc82ab
Mel Liu <privatemiao@gmail.com>
2016-05-16 16:51:41 +0800

    a

--- tutorial-shiro/src/test/java/org/mel/tutorial/shiro/CustomRealm.java
+++ tutorial-shiro/src/test/java/org/mel/tutorial/shiro/CustomRealm.java
@@ -4,22 +4,24 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
 public class CustomRealm extends AuthorizingRealm {
 
-	public void setName() {
-		super.setName("customRealm");
-	}
-
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		String userName = (String) token.getPrincipal();
-		String password = "P@ssw0rd";
-
-		return new SimpleAuthenticationInfo(userName, password, getName());
+		UsernamePasswordToken t = (UsernamePasswordToken) token;
+		System.out.println("Token->" + t);
+		System.out.println("getCredentials->" + new String((char[]) t.getCredentials()));
+		System.out.println("getHost->" + t.getHost());
+		System.out.println("getPassword->" + new String(t.getPassword()));
+		System.out.println("getPrincipal->" + t.getPrincipal());
+		System.out.println("getUsername->" + t.getUsername());
+		System.out.println("Name->" + getName());
+		return new SimpleAuthenticationInfo(t.getUsername(), new String(t.getPassword()), getName());
 	}
 
 	@Override
